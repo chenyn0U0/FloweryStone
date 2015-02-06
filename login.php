444f63f95cecd90202727c1b02130c70653da6ee
chen <ines.cyn@gmail.com>
2015-02-06 22:57:05 +0000

    I am a man without if le QUQ

--- login.php
+++ login.php
@@ -14,39 +14,34 @@ if (isset($_SESSION['phonenumber'])) {//如果已经登录了
 	header('Location: mainpage.php');
 }
 else if (isset($_POST['submit'])) {//提交电话号码后
-	$phoneNumber=$_POST['phonenumber'];
-
+	
 	//数据库操作（查询是否已有，已有登录，未有创建登录）
-	$con = mysql_connect("localhost","s1425535","InQzRF8RSnIf");
-	if (!$con)
-	  {
-	  die('Could not connect: ' . mysql_error());
-	  }
+	$con = new mysqli("localhost","s1425535","InQzRF8RSn","s1425535");
+	if ($con->connect_errno) {
+	   printf("Connect failed: %s\n", $con->connect_error);
+	   exit();
+	}
 
-	mysql_select_db("s1425535", $con);
-
-	$sqlsearch="SELECT * FROM `user` WHERE `phoneNumber` = $phoneNumber";
-
-
-	$result=mysql_query($sqlsearch);
-	$row = mysql_fetch_array($result);
+	$sqlsearch=$con->prepare("SELECT * FROM `user` WHERE `phoneNumber` = ?");
+	$sqlsearch->bind_param("s",$phoneNumber);
+	$phoneNumber=$_POST['phonenumber'];
+	$sqlsearch->execute();
+	$sqlsearch->close();
 	 
-	if($row==""){//不存在创建新用户
+	if(!$sqlsearch->fetch()){//不存在创建新用户
 		echo "not exist";
+
+		$sqladd=$con->prepare("INSERT INTO user (phoneNumber,registerTime,registerIP)
+								VALUES	(?,?,?)");
+		$sqladd->bind_param("sss",$phoneNumber,$registerTime,$registerIP);
+
 		$registerTime=date('Y-m-d H:i:s',time());
 		$registerIP=$_SERVER["REMOTE_ADDR"];
-
-		$sqladd="INSERT INTO user (phoneNumber,registerTime,registerIP)
-		VALUES
-		('$phoneNumber','$registerTime','$registerIP')";
-		if (!mysql_query($sqladd,$con))
-		  {
-		  die('Error: ' . mysql_error());
-		  }
-		echo "<hr/>1 record added";		
+		$sqladd->execute();
+		$sqladd->close();
 	}
 	else{echo "already exist";}//用户已存在
-	mysql_close($con);
+	$con->close();
 
 	$_SESSION['phonenumber'] = $_POST['phonenumber'];
 	header('Location: mainpage.php');
