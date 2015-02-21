5a04bdd4103e7284e144d9e8967ba7c3acd076b7
chen <ines.cyn@gmail.com>
2015-02-21 22:25:13 +0000

    house

--- PageOfHome.php
+++ PageOfHome.php
@@ -11,41 +11,108 @@
 	   exit();
 	}
 
-	$stmt=$con->prepare("SELECT bigmonsters.name,houseinfo.housepic FROM s1425535.bigmonsters , s1425535.houseinfo where bigmonsters.houseid=houseinfo.houseid and bigmonsters.ownerNum=".$_SESSION['phonenumber'].";");
+	$stmt=$con->prepare("SELECT bigmonsters.id,bigmonsters.name,houseinfo.housepic FROM s1425535.bigmonsters , s1425535.houseinfo where bigmonsters.houseid=houseinfo.houseid and bigmonsters.ownerNum=".$_SESSION['phonenumber'].";");
 	$stmt->execute();
-	$stmt->bind_result($monstername,$housesrc);
+	$stmt->bind_result($monsterid,$monstername,$housesrc);
 ?>
 	
-	<div style="width:100%; height:100%">
+<div  id="housesdiv" onmousemove="test(event)" style="overflow:hidden;width:960px; height:400px">
 
-<?php
+	<table  id="houses">
+		<tr>
+		
+			<?php
+				while ($stmt->fetch()) {
+				 printf("<td><a href=\"javascript:submit('%s')\";><div style='margin:50px; text-align:center' id='%s'><img style='height:180px;width:180px;' src='%s'/><p>%s</p></div></a></td>",$monsterid,$monstername,$housesrc,$monstername); 
+				}
+				$stmt->close();
+				$con->close();
+			?>
+		
+			<td>
+			<a href="javascript:alertname('newhouse');">
+				<div style='margin:50px; text-align:center' id='newhouse'>
+					<img style='height:180px;width:180px;' src='img/house/newhouse.png'/>
+					<p style=''>Adopt another lovely monster~</p>
+				</div>
+			</a>
+			</td>
+		</tr>
+	</table>
 
-
-?>
-
-<div>
-	
-	<?php
-	while ($stmt->fetch()) {
-	 printf("<a href=\"javascript:alertname('%s')\";><div style='float:left; margin:50px; text-align:center' id='%s'><img src='%s'/><p>%s</p></div></a>",$monstername,$monstername,$housesrc,$monstername); 
-	}
-	$stmt->close();
-	$con->close();
-	?>
-	
+	<form id="houseselection" action="monsterHome.php" method="post" style="display:none">
+	    <input type="hidden" name="monsterid" id="monsterid"/>
+	    <input type="submit" name="housesubmit" id="housesubmit"/>
+	</form>
 </div>
-<script>
-function alertname(name){
-	alert(name);
-}
+
+
+
+<script type="text/javascript">
+	function submit(id)
+	{
+	    document.getElementById("monsterid").value = id;
+	    document.getElementById("housesubmit").click();
+	}
 </script>
 
 
-<?php
+
+<script src="js/jquery.js"></script>
+
+<script type="text/javascript">
 
 
+	var width = $('#housesdiv').width(); 
+	var left = $('#housesdiv').offset().left; 
+	var scroll="none";
+	var ondiv=false;
+
+	setInterval("divscroll(scroll)", 100);
+
+	$('#housesdiv').hover(function(){ondiv=true;},function(){ondiv=false;})
+
+	function test(e){
+
+		var scrollborder1=0.1*width;
+		var scrollborder2=0.3*width;
+
+		var pointX = e.pageX;
+		var pointY = e.pageY;
 
 
-?>
+		var positiontoleft=pointX-left;
+		var positiontoright= left+width-pointX;
+		
 
-</div>
+		if(positiontoleft<scrollborder1) scroll="left1";
+		else if(positiontoleft<scrollborder2) scroll="left2";
+		else if(positiontoright<scrollborder1) scroll="right1";
+		else if(positiontoright<scrollborder2) scroll="right2";
+		else scroll="none";
+	}
+
+
+	function divscroll(scroll){
+		if(ondiv){ 
+			console.log(scroll);
+			if(scroll=="left1"){
+				var pos = $('#housesdiv').scrollLeft();
+				$('#housesdiv').animate({scrollLeft: (pos - 50)},98);
+			}
+			if(scroll=="left2"){
+				var pos = $('#housesdiv').scrollLeft();
+				$('#housesdiv').animate({scrollLeft: (pos - 20)},98);
+			}
+			if(scroll=="right1"){
+				var pos = $('#housesdiv').scrollLeft();
+				$('#housesdiv').animate({scrollLeft: (pos + 50)},98);
+			}
+			if(scroll=="right2"){
+				var pos = $('#housesdiv').scrollLeft();
+				$('#housesdiv').animate({scrollLeft: (pos + 20)},98);
+			}
+		}
+	}
+
+</script>
